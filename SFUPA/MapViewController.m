93e502618ba4bf8c878f3c011b7c4429a245fafe
Victor <victor@Victors-Mac-Pro.local>
2015-03-08 23:24:53 -0700

    Wayfinding for 2nd floor added (only works if to > from. ex: To: 2001, From: 2020

--- SFUPA/MapViewController.m
+++ SFUPA/MapViewController.m
@@ -11,12 +11,15 @@
 
 @interface MapViewController (){
     double num[1000];
+    double wayfind[1000];
     GMSMarker *marker;
     GMSCameraPosition *camera;
     CGFloat begLat;
     CGFloat begLong;
     CGFloat endLat;
     CGFloat endLong;
+    long to;
+    long from;
     IBOutlet GMSMapView *mapView;
     __weak IBOutlet UITextField *room;
     __weak IBOutlet UITextField *sroom;
@@ -35,27 +38,37 @@
     CGFloat latitude=0;
     CGFloat longtitude=0;
     bool exist = NO;
-    long temp;
-    temp=[room.text integerValue];
+    //long temp;
+    to=[room.text integerValue];
     
     //read file
     NSInteger index = 0;
     NSInteger count = 0;
+    
+    NSInteger wfIndex = 0;
+    
     while(index<1000)
     {
         //[test setText:[NSString stringWithFormat:@"%f", index]];
-        if (num[index] == temp){
+        if (num[index] == to){
             count=index;
             //[test setText:[NSString stringWithFormat:@"%f", num[index]]];
             exist=YES;
         }
         index++;
     }
+    while(wfIndex < 1000)
+    {
+        if (wayfind[wfIndex] == to){
+            break;
+        }
+        wfIndex = wfIndex + 3;
+    }
     if (exist == YES){
         latitude=num[count+1];
         longtitude=num[count+2];
-        endLat = latitude;
-        endLong = longtitude;
+        endLat = wayfind[wfIndex+1];
+        endLong = wayfind[wfIndex+2];
         camera = [GMSCameraPosition cameraWithLatitude:latitude
                                              longitude:longtitude
                                                   zoom:20];
@@ -67,8 +80,8 @@
         marker.map =mapView;
     }
     else{
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No such room"
-                                                        message:@"⚠"
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No such Room"
+                                                        message:@"Please Try Again."
                                                        delegate:nil
                                               cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
@@ -85,35 +98,51 @@
     CGFloat readLat=0;
     CGFloat readLong=0;
     bool exist = NO;
-    long temp;
-    temp=[sroom.text integerValue];
+    //long temp;
+    from=[sroom.text integerValue];
     
     //read file
     NSInteger index = 0;
     NSInteger count = 0;
+    
+    NSInteger wfIndex = 0;
+    
     while(index<1000)
     {
-        if (num[index] == temp){
+        if (num[index] == from){
             count=index;
             exist=YES;
-        
+            
         }
         index++;
+    }
+    
+    while(wfIndex <1000)
+    {
+        if (wayfind[wfIndex] == from){
+            break;
+        }
+        wfIndex = wfIndex + 3;
     }
     
     if (exist == YES){
         latitude=num[count+1];
         longtitude=num[count+2];
-        begLat = latitude;
-        begLong = longtitude;
+        //    begLat = latitude;
+        //   begLong = longtitude;
+        //  if (to > from)
+        //{
         while(readLat != endLat && readLong != endLong)
         {
-            readLat = num[count + 1];
-            readLong = num[count + 2];
+            readLat = wayfind[wfIndex + 1];
+            readLong = wayfind[wfIndex + 2];
             [path addCoordinate:CLLocationCoordinate2DMake(readLat, readLong)];
-            count = count + 3;
+            wfIndex = wfIndex + 3;
         }
-        [path addCoordinate:CLLocationCoordinate2DMake(endLat, endLong)];
+        // [path addCoordinate:CLLocationCoordinate2DMake(endLat, endLong)];
+        // }
+        
+        
         GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
         camera = [GMSCameraPosition cameraWithLatitude:latitude
                                              longitude:longtitude
@@ -124,6 +153,7 @@
         marker = [GMSMarker markerWithPosition:positon];
         marker.title = @"Begin";
         marker.map =mapView;
+        //mapView = GMSOverlay:polyline.map;
         polyline.map = mapView;
     }
     else{
@@ -166,8 +196,9 @@
     mapView.settings.myLocationButton = YES;
     //Sets the view controller to be the GMSMapView delegate
     mapView.delegate = self; //- See more at: http://vikrambahl.com/google-maps-ios-xcode-storyboards/#sthash.i08dW8lG.dpuf
-    room.placeholder = @"Room#:";
-    sroom.placeholder = @"Room#:";
+    //self.view = mapView;
+    //GMSMarker *marker = [[GMSMarker alloc] init];
+    //marker.position = CLLocationCoordinate2DMake(49.279937, -122.919956);
     
     //read file
     NSString *contents = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"AQ" ofType:@"txt"] encoding:NSUTF8StringEncoding error:NULL];
@@ -179,6 +210,15 @@
         CGFloat nse = [line floatValue];
         num[index]=nse;
         index++;
+        
+        NSString *wayfindFile = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"wayfind" ofType:@"txt"] encoding:NSUTF8StringEncoding error:NULL];
+        NSArray *wayfindLines = [wayfindFile componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+        NSInteger wfIndex = 0;
+        for (NSString *wfLines in wayfindLines){
+            CGFloat nse2 = [wfLines floatValue];
+            wayfind[wfIndex] = nse2;
+            wfIndex++;
+        }
     }
     
 }
@@ -189,4 +229,3 @@
 }
 
 @end
-
